d0ddb27d41c6da0fe54cbd25302254013a5c0a2a
HypoChloremic <26204559+HypoChloremic@users.noreply.github.com>
2020-06-15 12:48:26 +0200

    decoration entering and exiting print statements

---
+++ learning/decorators/stdout_logging.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+def stdout_log(func):
+    '''stdout_log() will print statements
+    upon entering and exiting a function, outputting the names of the 
+    functions that are being called'''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'[{func.__name__}] Entering')
+        result = func(*args, **kwargs)
+        print(f'[{func.__name__}] Exiting')
+        return result
+    return wrapper
+
+
+@stdout_log
+def randomfunc(printhtis='hello'):
+    return printthis
+
+class RandomClass:
+    def __init__(self):
+        pass
+
+    @stdout_log
+    def class_method(self):
+        return 'random class method'
